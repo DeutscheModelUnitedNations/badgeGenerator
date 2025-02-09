ec2c4990b28c732ec8762f1a54bc2af631de7b7e
Tade Strehk <git@strehk.eu>
2025-02-09 17:12:02 +0100

    ✨ feat: Refactor database initialization logic for improved clarity and error handling

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,25 +1,18 @@
 import type { Handle } from '@sveltejs/kit';
 import sqlite3 from 'sqlite3';
 
-export const handle: Handle = async ({ event, resolve }) => {
-	if (!event.locals.db) {
-		console.info('Database Hook: Creating database');
+const db = await new Promise<sqlite3.Database>((resolve) => {
+	function onResolve(err: Error | null) {
+		if (err) {
+			console.error('Database Hook: Error creating database', err);
+			throw err;
+		}
+		resolve(internaldb);
+	}
+	const internaldb = new sqlite3.Database('db.sqlite', onResolve);
+});
 
-		// This will create the database within the `db.sqlite` file.
-		const db = await new Promise<sqlite3.Database>((resolve) => {
-			function onResolve(err: Error | null) {
-				if (err) {
-					console.error('Database Hook: Error creating database', err);
-					throw err;
-				}
-				resolve(internaldb);
-			}
-			const internaldb = new sqlite3.Database('db.sqlite', onResolve);
-		});
-
-		event.locals.db = db;
-
-		console.info('Database Hook: Creating table');
+console.info('Database Hook: Creating table');
 		const query =
 			'CREATE TABLE IF NOT EXISTS image (title TEXT NOT NULL PRIMARY KEY, extension TEXT NOT NULL, image BLOB NOT NULL)';
 		await new Promise<void>(resolve => {
@@ -31,6 +24,10 @@ export const handle: Handle = async ({ event, resolve }) => {
 				resolve();
 			});
 		})
+
+export const handle: Handle = async ({ event, resolve }) => {
+	if (!event.locals.db) {
+		event.locals.db = db;
 	}
 	const resp = await resolve(event);
 	return resp;
